3dddc756fd66b60a6af9a32e8dcfb0da015cae53
Gian Carvalho <giandcarvalho@gmail.com>
2021-03-31 14:43:53 +0000

    Added JavaScript for sending card fields to stripe

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,36 @@
+/* global $, Stripe */
+//Document ready
+$(document).on('turbolinks:load', function(){
+  var theForm = $('#pro-form');
+  var submitBtn = $('form-signup-btn')
+  
+  //Set Stripe Public key
+  Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content'));
+  
+  //When user clicks form submit button
+  submitBtn.click(function(event){
+    //prevent default submission behavior
+    event.preventDefault();
+    
+    //Collect the credit card fields
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(), 
+        expYear = $('card_year').val();
+        
+    //Send the card information to Stripe
+    Stripe.createToken({
+      number: ccNum, 
+      cvc: cvcNum, 
+      exp_month: expMonth,
+      exp_year: expYear
+    }, stripeResponseHandler);
+  });
+  
+  
+  
+  
+  //Stripe will return a card token
+  //INject card token as hidden field into a form
+  //Submit form to our Rails app
+});
