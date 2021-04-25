a995d340993b2d899b9d88b26882a2071fe0728c
Gian Carvalho <giandcarvalho@gmail.com>
2021-04-25 19:03:17 +0000

    Added default avatar picture

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,6 @@ class Profile < ActiveRecord::Base
   
   has_attached_file :avatar, 
                     :styles => { :medium => "300x300>", :thumb => "100x100>"},
-                    :default_url => "/images/:style/missing.png" 
+                    :default_url => "https://www.attendit.net/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg" 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
